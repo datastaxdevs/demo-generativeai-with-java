786ad3d503b61a810d7c5d0127ffbe5f6c1af6af
clun <cedrick.lunven@gmail.com>
2023-09-12 11:36:35 +0200

    Ok

---
+++ genai-demo/src/test/java/fr/clunven/SampleTest.java
@@ -0,0 +1,20 @@
+package fr.clunven;
+
+import fr.clunven.todos.TodosSimpleRepository;
+import com.datastax.astra.sdk.AstraClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    AstraClient astraClient;
+    @Test
+    public void demoAstraClient() {
+        System.out.println("Connected to " + astraClient.apiDevops().getOrganization().getName());
+    }
+
+}
